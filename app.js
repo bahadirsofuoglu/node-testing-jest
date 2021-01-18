c6aacf6ee1014fdd15ac55f916bd53c60d17feb9
bahadirsofuoglu <bahadirsofuoglu@gmail.com>
2021-01-18 11:28:36 +0300

    routes added -nodemon added

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ let express = require('express')
 let port = 9000
 let app = express()
 
+let apiRoutes = require('./routes/api-routes')
 app.use(express.json())
-
+app.use('/api', apiRoutes)
 app.get('/', (req, res) => {
-  res.status(200)
+  res.status(200).json('hello')
 })
 
 app.listen(port, () => {
---
+++ routes/api-routes.js
@@ -0,0 +1,10 @@
+let router = require('express').Router()
+
+router.get('/', (req, res) => {
+  res.json({
+    status: 'API is working',
+    message: 'Hello'
+  })
+})
+
+module.exports = router
